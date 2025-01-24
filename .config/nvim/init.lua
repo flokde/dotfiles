9fc1cc5c650331a2439641860a66d192dc34fca2
Karl <krl@r12499.fritz.box>
2025-01-24 23:21:44 +0100

    added neovim files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,36 @@
+-- Set up lazy vim package manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- install plugins w/ lazy
+require('lazy').setup({
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
+})
+
+-- Set leader
+vim.g.mapleader = ','
+
+-- keep stuff in clipboard
+vim.keymap.set('n', 'p', 'P')
+
+-- redo
+vim.keymap.set('n', 'U', '<C-r>')
+
+-- Unhighlight w/ double leader
+vim.keymap.set('n', '<Leader>,', ':nohlsearch<cr>')
